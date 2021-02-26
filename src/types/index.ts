28b0b8c2876570d4968ae9c74f39ebbfaa625baf
yaeSakuras <363420113@qq.com>
2021-02-26 18:06:14 +0800

    完善工程

--- src/types/index.ts
+++ src/types/index.ts
@@ -45,6 +45,7 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+  defaults: AxiosRequestConfig
   interceptors: {
     request: AxiosInterceptorManager<AxiosRequestConfig>
     response: AxiosInterceptorManager<AxiosResponse>
